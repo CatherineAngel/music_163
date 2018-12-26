f191d14fc7434bb0b6f07a0d08994216540382ad
CatherineAngel <38883357+CatherineAngel@users.noreply.github.com>
2018-12-26 11:35:41 +0800

    创建请求头文件
    将不同的请求头放到同一个文件里，提升代码的可读性

---
+++ headers.py
@@ -0,0 +1,30 @@
+#歌曲评论headers请求头
+headers_discuss= {
+            'Accept':'*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Connection': 'keep-alive',
+            'Content-Length': '488',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cookie':'_iuqxldmzr_=32; _ntes_nnid=56f13d1f1f486e6766b92885b11f92ef,1545551450754; _ntes_nuid=56f13d1f1f486e6766b92885b11f92ef; WM_TID=2x6nxVuLYbJFUAAAAAdpe%2BPIAYqX%2BsXx; JSESSIONID-WYYY=fqOUBAUjsKJk1GQYHBOco%2FFhwcu6DukYyUJ%2FYfIU2ohHW2sNGUGXG8YJ2Y7a48WC4i%2Bc%2Fdsj8kRqGgP2%2FoTVQzOC9Gsq9MxD0bA5zEOv%5CFPx%2BV6rinRG7uqCOefcOIWkzpC%2Fvuk440%2B0kdmiXjI55rjfuwKlbbDCjVRpdOvHoIFYkvVa%3A1545736856527; __utma=94650624.1042807263.1545551453.1545650143.1545735057.5; __utmc=94650624; __utmz=94650624.1545735057.5.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; WM_NI=5lxQ6%2FWpLI8I%2B2VsYggHDUAlHym1aVzcXTsbJSHBcgQWtdT6qLAMN%2F4n5ilL7reukxmAEAKzDkZfyhKoOBK6nswZKxzDp4bFWH%2FqPfA%2FpbiODvj0lruZKGufNTb5HaxUVE4%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6ee98e460b0b99c87f733f7928ab2d55b869f9aabf36ab3a9a893f5538d949cb8c22af0fea7c3b92af7b4b8b2b52595b084aabb43e9ee9a94dc6da5988cb6c4688ca7a3d2e45ebabd98b5f26a8a8d88a3e26d9389bc85d73eb3948faecd25f8b2fb8aef48f5aeab96ea74af9f8ba2ca7288b59687fc6489b181a3ca3eaeadb9bbca74ba9cc0d5e14e9598e192d36e83f1c083ec4588aaa6d1ef7ab78685a8f86e8e8caed6cb6bbc8b9aa6b737e2a3; __utmb=94650624.20.10.1545735057',
+            'Host': 'music.163.com',
+            'Origin': 'https://music.163.com',
+            # 'Referer': 'https://music.163.com/song?id=326904',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+ }
+heades_songer_id={
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Connection': 'keep-alive',
+            'Content-Length': '408',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cookie': '_iuqxldmzr_=32; _ntes_nnid=56f13d1f1f486e6766b92885b11f92ef,1545551450754; _ntes_nuid=56f13d1f1f486e6766b92885b11f92ef; WM_TID=2x6nxVuLYbJFUAAAAAdpe%2BPIAYqX%2BsXx; __utmc=94650624; __utmz=94650624.1545735057.5.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; WM_NI=5lxQ6%2FWpLI8I%2B2VsYggHDUAlHym1aVzcXTsbJSHBcgQWtdT6qLAMN%2F4n5ilL7reukxmAEAKzDkZfyhKoOBK6nswZKxzDp4bFWH%2FqPfA%2FpbiODvj0lruZKGufNTb5HaxUVE4%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6ee98e460b0b99c87f733f7928ab2d55b869f9aabf36ab3a9a893f5538d949cb8c22af0fea7c3b92af7b4b8b2b52595b084aabb43e9ee9a94dc6da5988cb6c4688ca7a3d2e45ebabd98b5f26a8a8d88a3e26d9389bc85d73eb3948faecd25f8b2fb8aef48f5aeab96ea74af9f8ba2ca7288b59687fc6489b181a3ca3eaeadb9bbca74ba9cc0d5e14e9598e192d36e83f1c083ec4588aaa6d1ef7ab78685a8f86e8e8caed6cb6bbc8b9aa6b737e2a3; JSESSIONID-WYYY=k7O8Y6%2Brj%2F6VwzmzeF7GVVJlz19ozCuzU%2BFM6xZX2snK%5CXu2k1GPMROxwVu0sGkpU6Dt0llrbxUHJ3VAwJOwjZmiCiE8VNF955kgsc%5Cl3rpf%2BzYSC%2FT46NDpUbSQZmGufgWm%2BY16KrE6G0%2FkpHdZRsxIqvWUKbShs89CQ1%2FkRcHq965r%3A1545742413272; __utma=94650624.1042807263.1545551453.1545735057.1545741311.6; __utmb=94650624.6.10.1545741311',
+            'Host': 'music.163.com',
+            'Origin': 'https://music.163.com',
+            'Referer': 'https://music.163.com/discover/artist',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+
+
+
+}
